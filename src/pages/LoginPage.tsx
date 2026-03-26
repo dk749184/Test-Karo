91554cd22077fd00f7d5bf78d0c4e7080e6e2a5b
Deepak Kumar <149900780+dk749184@users.noreply.github.com>
2026-03-26 21:23:05 +0530

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -399,7 +399,7 @@ export function LoginPage({ onBackToHome }: LoginPageProps) {
           </form>
 
           {/* Demo Credentials */}
-          {isLogin && (
+          {/* {isLogin && (
             <div className="mt-4">
               <button
                 onClick={fillDemoCredentials}
@@ -408,7 +408,7 @@ export function LoginPage({ onBackToHome }: LoginPageProps) {
                 Use Demo Credentials ({role})
               </button>
             </div>
-          )}
+          )} */}
 
           {/* Toggle Login/Register */}
           <div className="mt-6 text-center">
